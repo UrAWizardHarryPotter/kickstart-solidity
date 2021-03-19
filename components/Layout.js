692cb862b74413200f93bff3a980b144569ce05f
UrAWizardHarryPotter <zach.schwartzman@gmail.com>
2021-03-19 11:10:24 -0400

    Added Form Error Handling and button loading spinners

--- components/Layout.js
+++ components/Layout.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import { Container } from 'semantic-ui-react';
+import Head from 'next/head';
 import Header from './Header';
 
 export default props => {
   return (
-      <Container>
-        <Header />
-        {props.children}
-      </Container>
+    <Container>
+      <Head>
+        <link
+          async
+          rel="stylesheet"
+          href="//cdn.jsdelivr.net/npm/semantic-ui@2.0.0/dist/semantic.min.css"
+        />
+      </Head>
+      <Header />
+      {props.children}
+    </Container>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -27,11 +27,6 @@ class CampaignIndex extends Component {
     return (
       <Layout>
         <div>
-          <link
-            async
-            rel="stylesheet"
-            href="//cdn.jsdelivr.net/npm/semantic-ui@2.0.0/dist/semantic.min.css"
-          />
           <h3>Open Campaigns</h3>
           <Button
             floated="right"
